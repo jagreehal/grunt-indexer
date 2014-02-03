2db3f0ce288a821ed3ed6db17a5c109970331937
Jag Reehal <jag.reehal@gmail.com>
2014-02-03 22:16:15 +0000

    added watch, stylish and load grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,68 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports=function(grunt){
+	// load all npm grunt tasks
+	require('load-grunt-tasks')(grunt);
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: '.jshintrc',
+				reporter: require('jshint-stylish')
+			}
+		},
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+		// Configuration to be run (and then tested).
+		indexer: {
+			default_options: {
+				options: {
+				},
+				files: {
+					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+				}
+			},
+			custom_options: {
+				options: {
+					separator: ': ',
+					punctuation: ' !!!'
+				},
+				files: {
+					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+				}
+			}
+		},
 
-    // Configuration to be run (and then tested).
-    indexer: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		},
+		watch: {
+			scripts: {
+				files: ['tasks/**/*.js', 'test/**/*.js'],
+				tasks: ['clean', 'indexer', 'nodeunit']
+			}
+		}
+	});
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  });
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'indexer', 'nodeunit']);
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'indexer', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test', 'watch']);
 
 };
